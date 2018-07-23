c701134868a73d9930cfa00631170e67bfb25dee
Harvey Yang <harveyyang2002@gmail.com>
2018-07-22 21:49:30 -0400

    Commit

--- hw4/hw4.R
+++ hw4/hw4.R
@@ -47,8 +47,8 @@ bind_and_shuffle = function(data, labels) {
 }
 
 train = function(data, labels, alpha){
-  threshold = 0.005
-  epochs = 100
+  threshold = 0.01
+  epochs = 20
   theta = as.matrix(runif(dim(data)[2], 0, 1))
   loss_old = Inf
   product = data %*% theta
@@ -60,8 +60,7 @@ train = function(data, labels, alpha){
     shuffled_data_labels = bind_and_shuffle(data, labels)
     data = shuffled_data_labels[, -786]
     labels = as.matrix(shuffled_data_labels[, 786])
-    theta_previous = theta
-    
+
     #Iterate over each sample in the dataset
     for (i in 1:dim(data)[1])
     {
@@ -90,27 +89,38 @@ accuracy = function(predicted_labels, true_labels) {
   return (acc)
 }
 
-model(train_data, train_labels, test_data, test_labels, alpha) {
+accuracy = function(predicted_labels, true_labels) {
+  acc = sum(predicted_labels == true_labels) / dim(true_labels)[1]
+  return (acc)
+}
+
+model = function(train_data, train_labels, test_data, test_labels, alpha) {
+  theta = train(train_data, train_labels, alpha)
+  prediction_train = predict(theta, train_data)
+  acc_train = accuracy(prediction_train, train_labels)
   
+  prediction_test = predict(theta, test_data)
+  acc_test = accuracy(prediction_test, test_labels)
+  return (list(theta, acc_train, acc_test))
 }
 
 
-theta = train(train_data_0_1, train_labels_0_1, 0.8)
-prediction = predict(theta, train_data_0_1)
-acc = accuracy(prediction, train_labels_0_1)
-sprintf("Accuracy of prediction in train_data_0_1 is %f", acc)
+theta1 = train(train_data_0_1, train_labels_0_1, 0.6)
+# prediction = predict(theta1, train_data_0_1)
+# acc = accuracy(prediction, train_labels_0_1)
+# sprintf("Accuracy of prediction in train_data_0_1 is %f", acc)
+# 
+# prediction = predict(theta1, test_data_0_1)
+# acc = accuracy(prediction, test_labels_0_1)
+# sprintf("Accuracy of prediction in test_data_0_1 is %f", acc)
 
-theta = train(train_data_0_1, train_labels_0_1, 0.8)
-prediction = predict(theta, test_data_0_1)
-acc = accuracy(prediction, test_labels_0_1)
-sprintf("Accuracy of prediction in test_data_0_1 is %f", acc)
+testing = model(train_data_0_1, train_labels_0_1, test_data_0_1, test_labels_0_1, 0.6)
 
-theta = train(train_data_3_5, train_labels_3_5, 0.8)
-prediction = predict(theta, train_data_3_5)
-acc = accuracy(prediction, train_data_3_5)
+theta2 = train(train_data_3_5, train_labels_3_5, 0.6)
+prediction = predict(theta2, train_data_3_5)
+acc = accuracy(prediction, train_labels_3_5)
 sprintf("Accuracy of prediction in train_data_3_5 is %f", acc)
 
-theta = train(train_data_3_5, train_labels_3_5, 0.8)
-prediction = predict(theta, test_data_3_5)
+prediction = predict(theta2, test_data_3_5)
 acc = accuracy(prediction, test_labels_3_5)
 sprintf("Accuracy of prediction in test_data_3_5 is %f", acc)
